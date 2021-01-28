3f32b157f0ed91174877f5c795be131bb4e89ef8
joseguevara29 <guevarabus29@gmail.com>
2021-01-27 23:40:42 -0500

    Sign up done needs styling, need to make log in.

---
+++ login-signup/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Signup from "./components/Signup";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Africa's Top Market Place</h1>
+      <Signup />
+    </div>
+  );
+}
+
---
+++ login-signup/src/components/Signup.js
@@ -0,0 +1,124 @@
+import React, { useState, useEffect } from "react";
+import Input from "./Input";
+import * as yup from "yup";
+import axios from "axios";
+
+export default function Signup() {
+  const defaultState = {
+    name: "",
+    password: "",
+    location: "",
+    accountType: "",
+    terms: false,
+  };
+
+  const [formState, setFormState] = useState(defaultState);
+  const [errors, setErrors] = useState({ ...defaultState, terms: "" });
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+
+  //Schema
+  // let signupSchema = yup.object().shape({
+  //   name: yup.string().required("Please provide your name."),
+  //   password: yup.string().required("Please make a new password"),
+  //   location: yup.string().optional(),
+  //   accountType: yup.string().required("Are you a seller or a buyer."),
+  //   terms: yup.boolean().oneOf([true], "Please agree to the terms and conditions."),
+  // })
+  let formSchema = yup.object().shape({
+    name: yup.string().required("Please provide name."),
+    password: yup.string().required("Please make a new password."),
+    location: yup.string().optional("This is oaptional."),
+    accountType: yup.string().required("Are you a seller or a buyer."),
+    terms: yup
+      .boolean()
+      .oneOf([true], "Please agree to the terms and conditions"),
+  });
+
+  useEffect(() => {
+    formSchema.isValid(formState).then((valid) => setButtonDisabled(!valid));
+  }, [formState]);
+
+  // onSubmit function
+  const formSubmit = (e) => {
+    e.preventDefault();
+    console.log("form submitted!", formState);
+    axios
+      .post("https://reqres.in/api/users", formState)
+      .then(() => console.log("form submitted success"))
+      .catch((err) => console.log(err));
+  };
+
+  //validate whether value meets schema
+  const validateChange = (e) => {
+    //this allows react to keep the event object to play nice with async op
+    //reach allows us to check a specific value of the schema
+    yup
+      .reach(formSchema, e.target.name)
+      .validate(e.target.value)
+      .then((valid) =>
+        setErrors({
+          ...errors,
+          [e.target.name]: "",
+        })
+      )
+      .catch((error) =>
+        setErrors({
+          ...errors,
+          [e.target.name]: error.errors[0],
+        })
+      );
+    e.persist();
+  };
+
+  // onChange function
+  const inputChange = (e) => {
+    const value =
+      e.target.type === "checkbox" ? e.target.checked : e.target.value;
+    setFormState({
+      ...formState,
+      [e.target.name]: value,
+    });
+    validateChange(e);
+  };
+
+  return (
+    <form onSubmit={formSubmit}>
+      <Input
+        type="text"
+        name="name"
+        onChange={inputChange}
+        value={formState.name}
+        label="Name"
+        errors={errors}
+      />
+      <Input
+        type="password"
+        name="password"
+        onChange={inputChange}
+        value={formState.password}
+        label="Password"
+        errors={errors}
+      />
+      <Input
+        type="text"
+        name="location"
+        onChange={inputChange}
+        value={formState.location}
+        label="Location"
+        errors={errors}
+      />
+      <label htmlFor="accountType">
+        <select name="accountType" onChange={inputChange}>
+          <option value="">--Please choose an option--</option>
+          <option value="Seller">Seller</option>
+          <option value="Buyer">Buyer</option>
+        </select>
+      </label>
+      <label className="terms" htmlFor="terms">
+        <input name="terms" type="checkbox" onChange={inputChange} />
+        Terms and Conditions
+      </label>
+      <button disabled={buttonDisabled}>Submit</button>
+    </form>
+  );
+}
